d3a40666fd246f129e2bf87de5ef8df3b9faa215
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-02 16:57:05 +0100

    choosing right data structure affect momory usage

--- right-data-structure/src/main/java/ma/youcode/App.java
+++ right-data-structure/src/main/java/ma/youcode/App.java
@@ -1,13 +1,40 @@
 package ma.youcode;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+/*
+        TODO:
+            Example:
+            -----------------------------------------------------------------------------
+                        Consume Less memory               Consume More Memory
+            -----------------------------------------------------------------------------
+                            ArrayList              vs       LinkedList
+                            HashSet                vs       TreeSet
+                            HashMap                vs       TreeMap
+                            StringBuilder          vs       String Concatenation
+                            Array                  vs       ArrayList
+                            Primitive Arrays:
+                            int[]   instead of Integer[]
+
+*/
+
+
+        System.out.println(palidromChecker("bob"));
+        System.out.println(palidromChecker("alex"));
+        System.out.println(palidromChecker("boob"));
+
+    }
+
+
+    private static boolean palidromChecker(String string){
+        int length = string.length();
+        for (int i = 0; i < length; i++) {
+             if(string.charAt(i)!=string.charAt(length-i-1)) return false;
+        }
+        return true;
     }
 }
