8a1a39fcb0c33ab8762aab00fcc94a32cdd34cee
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-02 10:54:05 +0100

    gc tunning as a technique to boost app performance

---
+++ garbage-collection/README.md
@@ -0,0 +1,11 @@
+
+
+
+
+### how to add VM options in IntelliJ IDEA
+> 1. open the `Run/Debug Configurations` dialog box
+> 2. select the configuration to change
+> 3. click the `Modify options` button
+> 4. select `Add VM options`
+> 5. enter the VM options
+![add_VM_options_image](./add_VM_options.png)
--- garbage-collection/src/main/java/ma/youcode/App.java
+++ garbage-collection/src/main/java/ma/youcode/App.java
@@ -1,13 +1,58 @@
 package ma.youcode;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class App
 {
+    /* TODO By default, the JVM uses the concurrent mark-and-sweep (CMS) collector in JVM v7,v8 and v9.
+        TODO Developers can also configure the garbage collector to use other algorithms, such as the parallel scavenge collector, which is better suited for multi-core systems.
+
+    /* TODO
+        Garbage Collection Tuning
+            consider using newer java versions to get better performance from the garbage collector.
+            -> it uses G1GC by default, which has a better performance than CMS.
+       Options :
+            Z Garbage Collector
+            G1 Garbage Collector
+            Parallel Garbage Collector
+            Concurrent Mark Sweep (CMS) Garbage Collector
+            Serial Garbage Collector
+            Parallel Old Garbage Collector
+            Parallel Scavenge Garbage Collector
+            Serial Old Garbage Collector
+    TODO
+     how to enable it ?
+        add one of these flags to the JVM arguments :
+            -XX:+UseG1GC
+            -XX:+UseParallelGC
+            -XX:+UseConcMarkSweepGC
+            -XX:+UseSerialGC
+            -XX:+UseParallelOldGC
+            -XX:+UseParNewGC
+            -XX:+UseSerialOldGC
+
+
+
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // print the current garbage collector
+        List<GarbageCollectorMXBean> beans =  ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean bean : beans) {
+            System.out.println("Garbage Collector Name: " + bean.getName());
+        }
     }
+
+
+    /*
+    TODO resources
+        Tunning Garbage collection
+     https://sematext.com/blog/java-garbage-collection-tuning/#:~:text=You%20can%20turn%20on%20the,the%20%2DXX%3A%2DUseParallelGC%20flag.
+        Understanding JMM
+     https://www.digitalocean.com/community/tutorials/java-jvm-memory-model-memory-management-in-java
+
+     */
 }
