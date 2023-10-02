447bb37b1f5afe1ba7dbb3c10648a5836041e0af
Mounir El Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-10-02 10:51:35 +0100

    add docs resources

--- object-pooling/src/main/java/ma/youcode/App.java
+++ object-pooling/src/main/java/ma/youcode/App.java
@@ -5,15 +5,14 @@ import org.apache.commons.pool2.impl.GenericObjectPool;
 public class App
 {
     /*
-    Object pooling is a technique where a pool of objects is maintained,
+    TODO
+     Object pooling is a technique where a pool of objects is maintained,
      and objects are reused from the pool instead of being created anew each time they’re needed.
      This can be an effective way to reduce memory usage and improve performance,
      particularly in situations where objects are frequently created and discarded.
-    Object pooling can be implemented using a variety of libraries, such as :
-
-     -> Apache Commons Pool
-     -> C3P0
-
+     Object pooling can be implemented using a variety of libraries, such as :
+        -> Apache Commons Pool
+        -> C3P0
    */
     public static void main( String[] args )
     {
@@ -21,4 +20,7 @@ public class App
         GenericObjectPool<StringBuffer> pool = new GenericObjectPool<>(object);
         ReaderUtil readerUtil = new ReaderUtil(pool);
     }
+    // TODO :
+    //  resources
+    //  https://softwareengineering.stackexchange.com/questions/115163/is-object-pooling-a-deprecated-technique
 }
